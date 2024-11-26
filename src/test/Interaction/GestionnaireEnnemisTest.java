c24b196217e9f0ef0bc67b46f5487861ceead554
caronf <florimond.caron@etu.umontpellier.fr>
2024-11-26 09:28:24 +0100

    create GestionnaireEnnemisTest

---
+++ src/test/Interaction/GestionnaireEnnemisTest.java
@@ -0,0 +1,12 @@
+package Interaction;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+public class GestionnaireEnnemisTest {
+
+    
+    
+}
