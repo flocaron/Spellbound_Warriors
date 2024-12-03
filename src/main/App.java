0024bd16fbaa20515c4a5effb7c570d373cd1f32
Hugo Decruyenaere <monkey_dhugo@air-de-hugo.home>
2024-12-03 09:02:00 +0100

    Test main et Modele

--- src/main/App.java
+++ src/main/App.java
@@ -1,4 +1,3 @@
-
 import Interaction.GestionnaireEnnemis;
 import Personnage.Heros;
 import Personnage.Modele.Ennemi;
@@ -10,7 +9,7 @@ public class App {
         System.out.println(" Caron Florimond");
         
         // Création du héros
-        Heros heros1 = new Heros("Sunraku", 250, 25, "Attaque Éclair", "Guerrier");
+        Heros heros1 = new Heros("Pikachu", 250, 25, "Attaque Éclair", "Guerrier");
 
         // Création du gestionnaire d'ennemis
         GestionnaireEnnemis gestionnaire = new GestionnaireEnnemis();
@@ -27,6 +26,10 @@ public class App {
             System.out.println("Un nouvel ennemi apparaît: " + ennemi.getNom());
 
             while (ennemi.estVivant() && heros1.estVivant()) {
+                // Demander la distance entre le héros et l'ennemi
+                System.out.print("Entrez la distance entre " + heros1.getNom() + " et " + ennemi.getNom() + ": ");
+                int distance = scanner.nextInt();
+
                 System.out.println("\nQue voulez-vous faire?");
                 System.out.print("1. Attaque Physique ");
                 System.out.print("2. Attaque Magique ");
@@ -38,24 +41,34 @@ public class App {
 
                 switch (choix) {
                     case 1 -> {
-                        // Attaque physique
-                        int degatsPhysiques = heros1.attaquePhysique();
-                        System.out.println(heros1.getNom() + " attaque " + ennemi.getNom() + " et inflige " + degatsPhysiques + " points de dégâts!");
-                        System.out.println(ennemi.recevoirDegats(degatsPhysiques));
+                        // Vérifie si l'ennemi est à portée pour une attaque physique
+                        if (heros1.getPorteeAttaque().estDansPortee(distance, "physique")) {
+                            // Effectuer l'attaque physique
+                            int degatsPhysiques = heros1.attaquePhysique();
+                            System.out.println(heros1.getNom() + " attaque " + ennemi.getNom() + " et inflige " + degatsPhysiques + " points de dégâts!");
+                            System.out.println(ennemi.recevoirDegats(degatsPhysiques));
+                        } else {
+                            System.out.println(ennemi.getNom() + " est trop loin pour une attaque physique.");
+                        }
                     }
                     case 2 -> {
                         // Attaque magique
-                        String resultatMagique = heros1.attaqueMagique();
-                        System.out.println(resultatMagique);
-                        if (heros1.getMana() >= 0) {
+                        if (heros1.getPorteeAttaque().estDansPortee(distance, "magique")) {
+                            String resultatMagique = heros1.attaqueMagique();
+                            System.out.println(resultatMagique);
                             System.out.println(ennemi.recevoirDegats(heros1.getForceAttaque() * 2)); // Dégâts de l'attaque magique
+                            System.out.println("Mana actuel de " + heros1.getNom() + ": " + heros1.getMana()); // Affiche le mana après l'attaque magique
+                        } else {
+                            System.out.println(ennemi.getNom() + " est trop loin pour une attaque magique.");
                         }
-                        System.out.println("Mana actuel de " + heros1.getNom() + ": " + heros1.getMana()); // Affiche le mana après l'attaque magique
                     }
                     case 3 -> {
                         // Utiliser capacité spéciale
                         if (!capaciteUtilisee) { // Vérifie si la capacité n'a pas été utilisée
-                            System.out.println(heros1.utiliserCapacite());
+                            System.out.print("Entrez la superficie de la salle: ");
+                            int superficieSalle = scanner.nextInt();
+                            System.out.println("Pkiachu utilise Attaque Éclaire");
+                            System.out.println(heros1.utiliserCapaciteSpeciale(superficieSalle));
                             capaciteUtilisee = true; // Marque la capacité comme utilisée
                         } else {
                             System.out.println("Vous avez déjà utilisé votre capacité spéciale!");
@@ -74,25 +87,12 @@ public class App {
                     System.out.println(heros1.recevoirDegats(degatsEnnemi));
                 } else {
                     System.out.println(ennemi.getNom() + " a été vaincu!");
-                    ennemisVaincus++; // Incrémente le compteur d'ennemis vaincus
-                    System.out.println("Ennemis vaincus: " + ennemisVaincus);
+                    ennemisVaincus++;
                 }
-            }
-
-            // Vérification si le héros est toujours vivant
-            if (!heros1.estVivant()) {
-                System.out.println(heros1.getNom() + " a été vaincu!");
-                continuer = false;
-            }
-
-            // Vérification si le nombre d'ennemis vaincus a atteint 3
-            if (ennemisVaincus >= 3) {
-                System.out.println("Félicitations! Vous avez vaincu 3 ennemis. Vous avez gagné!");
-                continuer = false; // Met fin au jeu
             }
         }
 
-        // Fermeture du scanner
+        System.out.println("Vous avez vaincu " + ennemisVaincus + " ennemis.");
         scanner.close();
     }
 }
